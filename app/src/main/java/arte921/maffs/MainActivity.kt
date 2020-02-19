c228bde25a5acc8e62f7b2fbc3042ea7dfdaee02
Ard-Jan van Etten <arte@localhost.localdomain>
2020-02-19 19:44:41 +0100

    Initial commit

---
+++ app/src/main/java/arte921/maffs/MainActivity.kt
@@ -0,0 +1,12 @@
+package arte921.maffs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
