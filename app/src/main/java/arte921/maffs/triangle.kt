6a2ee7db7303e7b859b8b4737a1c67db6cfee376
arte921 <ardjanve@gmail.com>
2020-02-20 15:02:58 +0100

    as

--- app/src/main/java/arte921/maffs/triangle.kt
+++ app/src/main/java/arte921/maffs/triangle.kt
@@ -1,7 +1,9 @@
 package arte921.maffs
 
 import java.lang.Math.PI
+import kotlin.math.acos
 import kotlin.math.asin
+import kotlin.math.pow
 import kotlin.math.sin
 
 class triangle (a: Double,b: Double,c: Double,alfa: Double,beta: Double,gamma: Double){
@@ -45,6 +47,7 @@ class triangle (a: Double,b: Double,c: Double,alfa: Double,beta: Double,gamma: D
                 }
                 //cosinusregel
 
+                if(a >= 0 && b >= 0 && c >= 0) alfa = acos((b.pow(2)+c.pow(2)-a.pow(2))/2*b*c); calculated = true
 
 
 
@@ -52,11 +55,39 @@ class triangle (a: Double,b: Double,c: Double,alfa: Double,beta: Double,gamma: D
             }
 
             if(beta<0){
+                if(alfa >= 0 || gamma >= 0) beta = PI - alfa - gamma; calculated = true
 
+                //sinusregel
+                if(b >= 0){
+                    if(c >= 0 && alfa >= 0){
+                        beta = asin(b*sin(alfa)/a)
+                        calculated = true
+                    }else if(a >= 0 && gamma >= 0){
+                        beta = asin(b*sin(gamma)/c)
+                        calculated = true
+                    }
+                }
+                //cosinusregel
+
+                if(b >= 0 && a >= 0 && c >= 0) beta = acos((a.pow(2)+c.pow(2)-b.pow(2))/2*a*c); calculated = true
             }
 
             if(gamma<0){
+                if(alfa >= 0 || beta >= 0) gamma = PI - alfa - beta; calculated = true
 
+                //sinusregel
+                if(c >= 0){
+                    if(b >= 0 && alfa >= 0){
+                        gamma = asin(c*sin(alfa)/a)
+                        calculated = true
+                    }else if(a >= 0 && beta >= 0){
+                        gamma = asin(c*sin(beta)/b)
+                        calculated = true
+                    }
+                }
+                //cosinusregel
+
+                if(c >= 0 && a >= 0 && b >= 0) gamma = acos((a.pow(2)+b.pow(2)-c.pow(2))/2*a*b); calculated = true
             }
         }
 
